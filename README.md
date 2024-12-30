af5bd5635539704e9ec30c3f26ada84cb4b7a585
Seya <31957516+sglkc@users.noreply.github.com>
2024-12-30 16:40:04 +0700

    ekstrak judul pdf dan update readme

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 # HerbalIR
 
-Data mining menggunakan scraper (cheerio)
-Information retrieval menggunakan full text search (flexsearch) dengan stemmer dan tokenizer Bahasa Indonesia (sastrawijs)
+Information retrieval dengan metode Fuzzy Full Text Search (minisearch)
+dengan stemmer dan tokenizer Bahasa Indonesia (sastrawijs)
+menggunakan dokumen PDF untuk mencari tanaman herbal.
 
 ## Cara menggunakan
 
-Sederhana saja
+### Clone repository
 
 ```sh
 git clone https://github.com/sglkc/herbalir.git
 cd herbalir
-npm i
-node . 'sakit perut'
 ```
+
+### Menambahkan dokumen
+
+1. Download dokumen berupa file PDF
+2. Simpan dokumen PDF ke folder `pdf`
+
+### Ekstraksi teks
+
+```sh
+python -m pip install -r requirements.txt
+python extractor.py
+```
+
+Hasil ekstraksi akan disimpan dalam file `data.json`. Ekstraksi judul tidak selalu benar, cek ulang data judul sebelum digunakan.
+
+### Membuka antarmuka
+
+1. Simpan folder ini dalam `htdocs` dalam XAMPP atau `www` dalam Laragon
+2. Jalankan web server Apache
+3. Buka `localhost/{nama folder}` atau domain dari Laragon
 
 ## Laporan ditunggu trims.
--- extractor.py
+++ extractor.py
@@ -8,19 +8,33 @@ def extract_pdf(pdf_path, output_txt_path, image_output_folder):
 
     # Initialize an empty string to hold the extracted text
     extracted_text = ""
+    title = ""
 
     # Iterate through each page in the PDF
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)  # Load the page
         text = page.get_text("text")  # Extract text from the page
         extracted_text += text + "\n"  # Add the text to the extracted_text string
+        largest_font_size = 0
 
         # Extract the first page as an image
         if page_num == 0:
             pix = page.get_pixmap() # type: ignore
             image_path = os.path.join(image_output_folder, os.path.splitext(os.path.basename(pdf_path))[0] + '.png')
             pix.save(image_path)
-            print(f"First page image has been successfully extracted from {pdf_path} and saved to {image_path}")
+            print(f"First page image has been successfully extracted from {pdf_path} and saved to {image_path}", flush=True)
+
+            text_blocks = page.get_text("dict")["blocks"]
+
+            # Loop through each text block
+            for block in text_blocks:
+                if "lines" in block:
+                    for line in block["lines"]:
+                        for span in line["spans"]:
+                            font_size = span["size"]
+                            if len(span['text']) > 2 and font_size > largest_font_size:
+                                largest_font_size = font_size
+                                title = span['text'].strip()
 
     # Close the PDF document
     pdf_document.close()
@@ -29,9 +43,9 @@ def extract_pdf(pdf_path, output_txt_path, image_output_folder):
     with open(output_txt_path, 'w', encoding='utf-8') as txt_file:
         txt_file.write(extracted_text)
 
-    print(f"Text has been successfully extracted from {pdf_path} and saved to {output_txt_path}")
+    print(f"Text has been successfully extracted from {pdf_path} and saved to {output_txt_path}", flush=True)
 
-    return pdf_path, output_txt_path, image_path
+    return title, pdf_path, output_txt_path, image_path
 
 def process_pdfs_in_folder(input_folder, output_folder, image_output_folder, json_output_path):
     # Ensure the output folders exist
@@ -50,8 +64,9 @@ def process_pdfs_in_folder(input_folder, output_folder, image_output_folder, jso
         if filename.lower().endswith('.pdf'):
             pdf_path = os.path.join(input_folder, filename)
             output_txt_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.txt')
-            pdf_path, output_txt_path, image_path = extract_pdf(pdf_path, output_txt_path, image_output_folder)
+            title, pdf_path, output_txt_path, image_path = extract_pdf(pdf_path, output_txt_path, image_output_folder)
             data["files"].append({
+                "title": title,
                 "pdf": pdf_path,
                 "text": output_txt_path,
                 "image": image_path
@@ -61,7 +76,7 @@ def process_pdfs_in_folder(input_folder, output_folder, image_output_folder, jso
     with open(json_output_path, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4)
 
-    print(f"File paths have been successfully saved to {json_output_path}")
+    print(f"File paths have been successfully saved to {json_output_path}", flush=True)
 
 # Example usage
 input_folder = 'pdf/'
